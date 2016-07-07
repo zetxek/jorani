99f2a1c8fe872e3b006d0eeeec9a460ed4f2dcb1
Adrián Moreno Peña <zetxek@gmail.com>
2016-07-07 10:50:28 +0200

    Safer way to create color column in the types table, as suggested by @bbalet

--- sql/patch_to_0.5.0.sql
+++ sql/patch_to_0.5.0.sql
@@ -1,11 +1,28 @@
 -- ---------------------------------------------------
 -- Jorani Schema upgrade to 0.5.0
--- 
+--
 -- @license      http://opensource.org/licenses/AGPL-3.0 AGPL-3.0
 -- @copyright  Copyright (c) 2014-2016 Benjamin BALET
 
 -- New features:
 --      * Bug fix on table dayoffs
+--      * Added new column "color" in the types table. Optional, will hold the color in the calendar for the approved requests of that type
+
+DELIMITER $$
+CREATE PROCEDURE sp_add_new_col_color()
+    SQL SECURITY INVOKER
+BEGIN
+        IF NOT EXISTS (
+                SELECT NULL
+                FROM information_schema.columns
+                WHERE table_schema = DATABASE() AND table_name ='types' AND column_name = 'color'
+        ) THEN
+                ALTER TABLE `types` ADD `color` varchar(7)  DEFAULT '' COMMENT 'non-required attribute to display a different color for the accepted leaves in the calendar';
+        END IF;
+END$$
+DELIMITER ;
+
+CALL sp_add_new_col_color();
+DROP PROCEDURE sp_add_new_col_color;
 
 ALTER TABLE `dayoffs` MODIFY `title` varchar(128) CHARACTER SET utf8;
-ALTER TABLE `types` ADD `color` varchar(7)  DEFAULT '' COMMENT 'non-required attribute to display a different color for the accepted leaves in the calendar';
